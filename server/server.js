080668f76f53aa990e73906dfaf381dd8ab4e831
Scott Everett <burningcupidxxx@gmail.com>
2023-11-21 21:38:37 -0800

    instruct server how to serve static assets in production mode

--- server/server.js
+++ server/server.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+  
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/'));
+  })
 
 app.use(routes);
 
